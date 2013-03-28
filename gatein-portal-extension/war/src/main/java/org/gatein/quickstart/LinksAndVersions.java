20f43a3379001f62502f0ac125c7c5307ec8ea9b
Peter Palaga <ppalaga@redhat.com>
2013-03-28 12:00:05 +0100

    GTNPORTAL-2870 In quickstarts generate Java constants for external link together with READMEs

---
+++ gatein-portal-extension/war/src/main/java/org/gatein/quickstart/LinksAndVersions.java
@@ -0,0 +1,85 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gatein.quickstart;
+
+/**
+ * A helper class generated through <code>mvn install -P generate-readmes</code>.
+ * The constants in this class mirror properties from top pom.xml file.
+ *
+ * @author Peter Palaga
+ *
+ */
+public class LinksAndVersions {
+    public static final String compatibility_as_major_version = "7";
+    public static final String compatibility_as_minor_version = "1";
+    public static final String compatibility_as_product = "JBoss AS";
+    public static final String compatibility_as_productAndVersion = "JBoss AS 7.1";
+    public static final String compatibility_as_version = "7.1";
+    public static final String compatibility_as_versionMicro = "1";
+    public static final String compatibility_as_versionMmm = "7.1.1";
+    public static final String compatibility_portal_community_projectName = "GateIn Portal";
+    public static final String compatibility_portal_community_projectNameAndVersion = "GateIn Portal 3.6";
+    public static final String compatibility_portal_community_projectNameShort = "GateIn";
+    public static final String compatibility_portal_community_versionMajor = "3";
+    public static final String compatibility_portal_community_versionMicro = "0";
+    public static final String compatibility_portal_community_versionMinor = "6";
+    public static final String compatibility_portal_community_versionMm = "3.6";
+    public static final String compatibility_portal_community_versionMmm = "3.6.0";
+    public static final String compatibility_portal_product_projectName = "JBoss Portal Platform (JPP)";
+    public static final String compatibility_portal_product_projectNameAndVersion = "JBoss Portal Platform (JPP) 6.0";
+    public static final String compatibility_portal_product_projectNameShort = "JPP";
+    public static final String compatibility_portal_product_versionMajor = "6";
+    public static final String compatibility_portal_product_versionMicro = "0";
+    public static final String compatibility_portal_product_versionMinor = "0";
+    public static final String compatibility_portal_product_versionMm = "6.0";
+    public static final String compatibility_portal_product_versionMmm = "6.0.0";
+    public static final String compatibility_portal_projectName = "GateIn Portal";
+    public static final String compatibility_portal_projectNameAndVersion = "GateIn Portal 3.6";
+    public static final String compatibility_portal_projectNameShort = "GateIn";
+    public static final String compatibility_portal_versionMajor = "3";
+    public static final String compatibility_portal_versionMicro = "0";
+    public static final String compatibility_portal_versionMinor = "6";
+    public static final String compatibility_portal_versionMm = "3.6";
+    public static final String compatibility_portal_versionMmm = "3.6.0";
+    public static final String compatibility_wfk_versionMajor = "2";
+    public static final String compatibility_wfk_versionMicro = "0";
+    public static final String compatibility_wfk_versionMinor = "1";
+    public static final String compatibility_wfk_versionMmm = "2.1.0";
+    public static final String gatein_devguide_containers_url = "https://docs.jboss.org/author/display/GTNPORTAL36/GDG-Portal+Containers+and+Extensions";
+    public static final String gatein_devguide_custom_groovy_template_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Custom+Groovy+Template+for+a+Portlet";
+    public static final String gatein_devguide_custom_portlet_skin_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Custom+Skin+for+a+Portlet";
+    public static final String gatein_devguide_extensions_url = "https://docs.jboss.org/author/display/GTNPORTAL36/GDG-Portal+Containers+and+Extensions";
+    public static final String gatein_devguide_i18n_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Custom+Internationalization+Resource+Bundles";
+    public static final String gatein_devguide_jsf_basic_portlet_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Basic+JSF+Portlet+Development";
+    public static final String gatein_devguide_jsf_plain_jsr268_portlet_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Standard+Portlet+Development+%28JSR-286%29";
+    public static final String gatein_devguide_jsf_portlets_url = "https://docs.jboss.org/author/display/GTNPORTAL36/JSF2+Portlet+Development";
+    public static final String gatein_devguide_jsf_rf_portlet_url = "https://docs.jboss.org/author/display/GTNPORTAL36/JSF+portlet+development+with+RichFaces";
+    public static final String gatein_devguide_nav_pages_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Custom+Navigation+and+Pages";
+    public static final String gatein_devguide_starting_portlet_project_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Starting+a+Portlet+Project";
+    public static final String gatein_devguide_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Developer+Guide";
+    public static final String gatein_docs_url = "https://docs.jboss.org/author/display/GTNPORTAL36";
+    public static final String gatein_quickstarts_downloads_url = "https://github.com/downloads/gatein/gatein-portal-quickstart";
+    public static final String gatein_refguide_pbr_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Reference_Guide/chap-Reference_Guide-Building_JSF_Portlets.html";
+    public static final String gatein_refguide_portlet_primer_url = "https://docs.jboss.org/author/display/GTNPORTAL36/Portlet+Primer";
+    public static final String gatein_user_forum_url = "https://community.jboss.org/en/gatein?view=discussions";
+    public static final String jboss_admin_guide_ds_files_url = "https://docs.jboss.org/author/display/AS71/Admin+Guide#AdminGuide-Deploymentof%5Cds.xmlfiles";
+    public static final String jboss_admin_guide_ds_url = "https://docs.jboss.org/author/display/AS71/Admin+Guide#AdminGuide-Datasources";
+    public static final String jboss_admin_guide_url = "https://docs.jboss.org/author/display/AS71/Admin+Guide";
+    public static final String jboss_confluence_root_url = "https://docs.jboss.org/author/display";
+    public static final String mvnrepo_redhat_all_url = "http://maven.repository.redhat.com/techpreview/all";
+}
